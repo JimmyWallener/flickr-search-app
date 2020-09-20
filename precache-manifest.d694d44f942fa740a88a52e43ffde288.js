d677febd461a15c34b374dede28393bbb6b910cd
Jimmy Wallener <jimmy.wallener@protonmail.com>
2020-09-20 12:18:37 +0200

    Updates

---
+++ precache-manifest.d694d44f942fa740a88a52e43ffde288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d47d6b0c3e3845b1987afbe60784d63",
+    "url": "/flickr-search-app/index.html"
+  },
+  {
+    "revision": "7ab7488fe390b91c9f0d",
+    "url": "/flickr-search-app/static/css/main.268f27eb.chunk.css"
+  },
+  {
+    "revision": "60d125aaee4b355c8549",
+    "url": "/flickr-search-app/static/js/2.29bfa278.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flickr-search-app/static/js/2.29bfa278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab7488fe390b91c9f0d",
+    "url": "/flickr-search-app/static/js/main.681dfdb9.chunk.js"
+  },
+  {
+    "revision": "5cf1e7f2df87f39beff4",
+    "url": "/flickr-search-app/static/js/runtime-main.d596a148.js"
+  }
+]);
